8a88f4f4083226026cbf9df5bb6fd56135562332
Evgeniy <evgeniipusser88@gmail.com>
2025-02-20 17:09:36 +0200

    my version of sorting plus map-join instrument

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-const arr = ["Uga", "java", 78,  "Javascript", "NODE", 128, "Hello", "fraction", 1, -90];
+const arr = [
+  "Uga",
+  "java",
+  78,
+  "Javascript",
+  "NODE",
+  128,
+  "Hello",
+  "fraction",
+  1,
+  -90,
+];
 
-function compNumStr(e1, e2) {
-  if (typeof e1 === 'number' && typeof e2 === 'string') {
-    return -1;
-  } else if (typeof e1 === 'string' && typeof e2 === 'number') {
-    return 1;
-  } else if (typeof e1 === 'number' && typeof e2 === 'number') {
-    return e2 - e1;
-  } else if (typeof e1 === 'string' && typeof e2 === 'string') {
-    if (e1 < e2) {
-      return -1;
-    } else if (e1 > e2) {
-      return 1;
-    } else {
-      return 0;
-    }
-    return 0;
+function compNumStr(arr) {
+  arr.sort();
+
+  const indOfStr = arr.findIndex(function (element) {
+    return typeof element === "string";
+  });
+
+  let numbers = arr.slice(0, indOfStr).sort(function (a, b) {
+    return b - a;
+  });
+
+  for (var i = 0; i < numbers.length; i++) {
+    arr[i] = numbers[i];
   }
 }
 
-arr.sort(compNumStr);
+compNumStr(arr);
 console.log(arr);
 
+function orderedList(array) {
+  return `
+        <ol>
+            ${array
+              .map(function (item) {
+                return `<li class="item${
+                  typeof item === "number" ? " item_number" : ""
+                }">${item}</li>`;
+              })
+              .join("")}
+        </ol>
+    `;
+}
 
-arrHTML = [2, "About you & me", 1, "Hello", 3, "World"];
-arrHTML.map(function (element) {
-  if (typeof element === 'string') {
-    let res = '<li class="item">element</li>';
-  }
-  else if (typeof element === 'number') {
-    let res = '<li class="item item_number">element</li>';
-  }
-});
-
- const str = arrHTML.join("<br>");
-console.log(str);
+const bodyElement = document.querySelector("body");
+bodyElement.innerHTML = orderedList([
+  "I can understand nothing still",
+  3900,
+  -90.888,
+  "Nope Java",
+  "To be or not to be?",
+  1000000,
+]);
